b85f74c2c125d35999833eefe0c14fc16fb15a21
idair <idair@10.236.63.93>
2025-05-07 17:35:17 +0200

    ALGO

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/CartaNautica/FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         stage.getIcons().add(new Image(getClass().getResourceAsStream("/resources/logo.png")));
         Scene scene = new Scene(root);
         stage.setTitle("Puntos de interes UPV");
