e9000376c008b949f23b06e8c8af2586b38f0dd2
Sibrix <154474179+Sibrix@users.noreply.github.com>
2025-05-08 09:38:55 +0200

    INI

--- src/Main/MainController.java
+++ src/Main/MainController.java
@@ -4,16 +4,30 @@
  */
 package Main;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.application.Application;
 
 /**
  * FXML Controller class
  *
  * @author idair
  */
-public class MainController implements Initializable {
+public class MainController implements Initializable   {
+
+    @FXML
+    private Button ini;
 
     /**
      * Initializes the controller class.
@@ -22,5 +36,16 @@ public class MainController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void inicio_de_sesion(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("iniciarSesion.fxml"));
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/resources/logo.png")));
+        Scene scene = new Scene(root);
+        stage.setTitle("Puntos de interes UPV");
+        stage.setScene(scene);
+        stage.show();
+    }
     
 }
